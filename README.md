f6915eb064ff0d1afb9ee4fe34d56ca14e6e57b7
Evert Albert <evert.albert@hotmail.be>
2022-01-16 16:04:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot_content_test
---
+++ src/main/java/com/example/servecontent/ServecontentApplication.java
@@ -0,0 +1,13 @@
+package com.example.servecontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServecontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServecontentApplication.class, args);
+	}
+
+}
